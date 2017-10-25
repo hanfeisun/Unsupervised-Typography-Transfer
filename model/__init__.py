Fix the GAN formula

model/__init__.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
 
+G_PREFIX = "generator"
+D_PREFIX = "discriminator"
+
 
 def model_fn(features, labels, mode, params):
     def conv(feature, num_outputs, kernel_size, stride, scope_name, reuse=False, padding='same'):
@@ -16,31 +19,32 @@ def model_fn(features, labels, mode, params):
 
     # data_format is (batch, height, width, channels)
 
-    e1 = conv(features['source'], 64, 3, 1, 'e1')
-    e2 = conv(e1, 64, 3, 2, 'e2')
-    e3 = conv(e2, 128, 3, 1, 'e3')
-    e4 = conv(e3, 128, 3, 2, 'e4')
-    e5 = conv(e4, 256, 3, 1, 'e5')
-    e6 = conv(e5, 256, 3, 2, 'e6')
-    e7 = conv(e6, 512, 3, 1, 'e7')
-    e8 = conv(e7, 512, 3, 2, 'e8')
+    with tf.variable_scope(G_PREFIX):
+        e1 = conv(features['source'], 64, 3, 1, 'e1')
+        e2 = conv(e1, 64, 3, 2, 'e2')
+        e3 = conv(e2, 128, 3, 1, 'e3')
+        e4 = conv(e3, 128, 3, 2, 'e4')
+        e5 = conv(e4, 256, 3, 1, 'e5')
+        e6 = conv(e5, 256, 3, 2, 'e6')
+        e7 = conv(e6, 512, 3, 1, 'e7')
+        e8 = conv(e7, 512, 3, 2, 'e8')
 
-    d1 = deconv(e8, 512, 3, 2, 'd1')
-    d2 = conv(tf.concat([e7, d1], axis=3), 512, 3, 1, 'd2')
-    d3 = conv(d2, 256, 3, 1, 'd3')
-    d4 = deconv(d3, 256, 3, 2, 'd4')
-    d5 = conv(tf.concat([e5, d4], axis=3), 256, 3, 1, 'd5')
-    d6 = conv(d5, 128, 3, 1, 'd6')
-    d7 = deconv(d6, 128, 3, 2, 'd7')
-    d8 = conv(tf.concat([e3, d7], axis=3), 128, 3, 1, 'd8')
-    d9 = conv(d8, 64, 3, 1, 'd9')
-    d10 = deconv(d9, 64, 3, 2, 'd10')
-    d11 = conv(tf.concat([e1, d10], axis=3), 64, 3, 1, 'd11')
-    d12 = conv(d11, 1, 3, 1, 'd12')
-    generated = tf.nn.sigmoid(d12, 'gen')
+        d1 = deconv(e8, 512, 3, 2, 'd1')
+        d2 = conv(tf.concat([e7, d1], axis=3), 512, 3, 1, 'd2')
+        d3 = conv(d2, 256, 3, 1, 'd3')
+        d4 = deconv(d3, 256, 3, 2, 'd4')
+        d5 = conv(tf.concat([e5, d4], axis=3), 256, 3, 1, 'd5')
+        d6 = conv(d5, 128, 3, 1, 'd6')
+        d7 = deconv(d6, 128, 3, 2, 'd7')
+        d8 = conv(tf.concat([e3, d7], axis=3), 128, 3, 1, 'd8')
+        d9 = conv(d8, 64, 3, 1, 'd9')
+        d10 = deconv(d9, 64, 3, 2, 'd10')
+        d11 = conv(tf.concat([e1, d10], axis=3), 64, 3, 1, 'd11')
+        d12 = conv(d11, 1, 3, 1, 'd12')
+        generated = tf.nn.sigmoid(d12, 'gen')
 
     def discriminator(image, reuse=False):
-        with tf.variable_scope('discriminator', reuse=reuse):
+        with tf.variable_scope(D_PREFIX, reuse=reuse):
             net = conv(image, 2, 3, 1, 'dis_conv1')
             net = conv(net, 4, 3, 2, 'dis_conv2')
             net = conv(net, 8, 3, 2, 'dis_conv3')
@@ -64,7 +68,8 @@ def model_fn(features, labels, mode, params):
     d12_flatten = tf.reshape(d12, [-1, 64 * 64])
     target_flatten = tf.reshape(real_target, [-1, 64 * 64])
 
-    g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=d12_flatten, labels=target_flatten))
+    g_loss = tf.reduce_mean(
+        tf.nn.sigmoid_cross_entropy_with_logits(logits=d12_flatten, labels=target_flatten)) - 0.5 * d_loss_fake
 
     learning_rate = params['learning_rate']
 
@@ -73,11 +78,20 @@ def model_fn(features, labels, mode, params):
     tf.summary.image("original", features['source'])
     tf.summary.image("target", features['target'])
     tf.summary.image("transfer", generated)
-    loss = g_loss + d_loss * 0.5
-    # loss = d_loss + g_loss
-    train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+    loss = g_loss + d_loss
 
-    logging_hook = tf.train.LoggingTensorHook({"loss": loss, "g_loss": g_loss, "d_loss": d_loss}, every_n_iter=100)
+    theta_d = []
+    theta_g = []
+    for v in tf.trainable_variables():
+        if v.name.startswith(D_PREFIX):
+            theta_d.append(v)
+        elif v.name.startswith(G_PREFIX):
+            theta_g.append(v)
+
+    d_train = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(d_loss, var_list=theta_d)
+    g_train = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(g_loss, var_list=theta_g)
+
+    logging_hook = tf.train.LoggingTensorHook({"g_loss": g_loss, "d_loss": d_loss}, every_n_iter=10)
     summary_hook = tf.train.SummarySaverHook(
         save_secs=5,
         output_dir="./board",
@@ -86,6 +100,6 @@ def model_fn(features, labels, mode, params):
     return tf.estimator.EstimatorSpec(
         mode=mode,
         loss=loss,
-        train_op=train_op,
+        train_op=tf.group(d_train, g_train),
         training_hooks=[logging_hook, summary_hook]
     )