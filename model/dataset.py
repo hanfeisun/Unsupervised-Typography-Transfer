46dc1626423fd4ec5051312415ff128cd3ce88d4
Hanfei Sun <ad9075@gmail.com>
2017-11-06 18:01:28 -0500

    Add PairPolicy parameter and implement SOFT_PAIR_MINUS

--- model/dataset.py
+++ model/dataset.py
@@ -3,6 +3,16 @@ import numpy as np
 import tensorflow as tf
 import random
 import os
+from enum import Enum
+
+
+class PairPolicy(Enum):
+    STRONG_PAIR = 1
+    SOFT_PAIR_MINUS = 2
+    SOFT_PAIR_PLUS = 3
+    NO_PAIR = 4
+
+
 from .utils import bytes_to_file, \
     read_split_image, shift_and_resize_image, normalize_image
 
@@ -41,19 +51,32 @@ def process(img):
         img.close()
 
 
-def input_fn(path, shuffle=True, num_epochs=None):
+def input_fn(path, shuffle=True, num_epochs=None, pair_policy=PairPolicy.STRONG_PAIR):
     pickled = PickledImageProvider(path)
-
     processed = [np.array(process(e[1])).astype(np.float32) for e in pickled.examples]
     x = {
         'source': np.array([img[0] for img in processed]),
         'target': np.array([img[1] for img in processed])
     }
 
-    return tf.estimator.inputs.numpy_input_fn(
-        x=x,
-        y=None,
-        shuffle=shuffle,
-        num_epochs=num_epochs,
-        batch_size=8
-    )
+    if pair_policy is PairPolicy.STRONG_PAIR:
+        return tf.estimator.inputs.numpy_input_fn(
+            x=x,
+            y=None,
+            shuffle=shuffle,
+            num_epochs=num_epochs,
+            batch_size=8
+        )
+    elif pair_policy is PairPolicy.SOFT_PAIR_MINUS:
+        np.random.shuffle(x['target'])
+        return tf.estimator.inputs.numpy_input_fn(
+            x=x,
+            y=None,
+            shuffle=shuffle,
+            num_epochs=num_epochs,
+            batch_size=8
+        )
+    elif pair_policy is PairPolicy.SOFT_PAIR_PLUS:
+        raise NotImplemented
+
+
