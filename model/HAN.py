05052d2c32b7e9e23bf8de8d9613559cced22bf1
Ziang Lu <ziangl@andrew.cmu.edu>
2017-12-08 13:16:19 -0500

    refactored

--- model/HAN.py
+++ model/HAN.py
@@ -1,35 +1,50 @@
 import tensorflow as tf
+from pdb import set_trace
 
 G_PREFIX = "generator"
 D_PREFIX = "discriminator"
 
 
 def model_fn(features, labels, mode, params):
-
-    def conv(feature, num_outputs, kernel_size, stride, scope_name, reuse=False, padding='same', BN=True):
+    def conv(feature, num_outputs, kernel_size, stride, scope_name, reuse=False,
+             padding='same', BN=True):
         # construct a convolutional layer group
         with tf.variable_scope(scope_name, reuse=reuse):
-            conv_x = tf.layers.conv2d(feature, num_outputs, kernel_size, stride, padding, name='conv')
+            conv_x = tf.layers.conv2d(feature, num_outputs,
+                kernel_size=kernel_size, strides=stride, padding=padding,
+                name='conv')
             if BN:
-                conv_x = tf.layers.batch_normalization(conv_x, name='batchnorm',
-                                                       training=(mode is tf.estimator.ModeKeys.TRAIN))
+                if mode == tf.estimator.ModeKeys.TRAIN:
+                    conv_x = tf.layers.batch_normalization(conv_x,
+                        training=True, name='batchnorm')
+                else:
+                    conv_x = tf.layers.batch_normalization(conv_x,
+                        name='batchnorm')
             conv_x = tf.nn.elu(conv_x, name='elu')
         return conv_x
 
-    def deconv(feature, num_outputs, kernel_size, stride, scope_name, reuse=False, padding='same', BN=True):
+    def deconv(feature, num_outputs, kernel_size, stride, scope_name,
+               reuse=False, padding='same', BN=True):
         # construct a deconvolutional layer group
         with tf.variable_scope(scope_name, reuse=reuse):
-            deconv_x = tf.layers.conv2d_transpose(feature, num_outputs, kernel_size, stride, padding, name='deconv')
+            deconv_x = tf.layers.conv2d_transpose(feature, num_outputs,
+                kernel_size=kernel_size, strides=stride, padding=padding,
+                name='deconv')
             if BN:
-                deconv_x = tf.layers.batch_normalization(deconv_x, name='batchnorm',
-                                                         training=(mode is tf.estimator.ModeKeys.TRAIN)                                                        )
+                if mode == tf.estimator.ModeKeys.TRAIN:
+                    deconv_x = tf.layers.batch_normalization(deconv_x,
+                        training=True, name='batchnorm')
+                else:
+                    deconv_x = tf.layers.batch_normalization(deconv_x,
+                        name='batchnorm')
         return deconv_x
 
     # data_format is (batch, height, width, channels)
 
     with tf.variable_scope(G_PREFIX):
         # architecture picture: https://www.dropbox.com/s/1xjzj7u1nf4x09k/IMG_0073.JPG?dl=0
-        e1 = conv(features['source'], 64, 3, 1, 'e1')
+        e1 = conv(features['source'], num_outputs=64, kernel_size=3, stride=1,
+                  scope_name='e1')
         e2 = conv(e1, 64, 3, 2, 'e2')
         e3 = conv(e2, 128, 3, 1, 'e3')
         e4 = conv(e3, 128, 3, 2, 'e4')
@@ -74,37 +89,61 @@ def model_fn(features, labels, mode, params):
 
     # real samples should be classified as one
     d_loss_real = tf.reduce_mean(
-        tf.nn.sigmoid_cross_entropy_with_logits(logits=real_logits, labels=tf.ones_like(real_logits)))
+        tf.nn.sigmoid_cross_entropy_with_logits(logits=real_logits,
+                                                labels=tf.ones_like(real_logits)))
     # real samples should be classified as zero
     d_loss_fake = tf.reduce_mean(
-        tf.nn.sigmoid_cross_entropy_with_logits(logits=fake_logits, labels=tf.zeros_like(fake_logits)))
+        tf.nn.sigmoid_cross_entropy_with_logits(logits=fake_logits,
+                                                labels=tf.zeros_like(fake_logits)))
+    d_loss = d_loss_real + d_loss_fake
 
     d12_flatten = tf.reshape(d12, [-1, 64 * 64])
     target_flatten = tf.reshape(real_target, [-1, 64 * 64])
 
-    d_loss = d_loss_real + d_loss_fake
-
     # Use cross entropy as pixel loss.
     # L1 or L2 loss may also be considerable.
     pixel_loss = tf.reduce_mean(
-        tf.nn.sigmoid_cross_entropy_with_logits(logits=d12_flatten, labels=target_flatten))
+        tf.nn.sigmoid_cross_entropy_with_logits(logits=d12_flatten,
+                                                labels=target_flatten))
     g_loss = pixel_loss - d_loss_fake
 
+    ##### Construct e1 batch images
+    def get_img(e):
+        e1_img = get_row(e, 0)
+        for grid_row in range(1, 8):
+            row = get_row(e, grid_row)
+            e1_img = tf.concat([e1_img, row], axis=0)
+        e1_img = tf.reshape(e1_img, [512, 512, 1])
+        return e1_img
+
+    def get_row(e, grid_row):
+        start = grid_row * 8
+        row = e[start]
+        for i in range(1, 8):
+            row = tf.concat([row, e[start + i]], axis=1)
+        return row
+
+    e1_batch_imgs = []
+    for i in range(8):
+        e1_img = get_img(e1[i])
+        e1_batch_imgs.append(e1_img)
+    e1_batch_imgs = tf.convert_to_tensor(e1_batch_imgs)
+
     if mode == tf.estimator.ModeKeys.TRAIN:
+        tf.summary.scalar("d_fake_loss", d_loss_fake)
         tf.summary.scalar("d_loss", d_loss)
-        tf.summary.scalar("fake_loss", d_loss_fake)
-        tf.summary.scalar("g_loss", g_loss)
         tf.summary.scalar("pixel_loss", pixel_loss)
+        tf.summary.scalar("g_loss", g_loss)
         tf.summary.image("original", features['source'])
         tf.summary.image("target", features['target'])
-        tf.summary.image("transfer_sigmoid", generated)
-
+        tf.summary.image("transfered", generated)
+        tf.summary.image("conv1", e1_batch_imgs) ##### Added
     else:
         # EVAL
         tf.summary.scalar("cross_entropy_loss", pixel_loss)
         tf.summary.image("eval_original", features['source'], max_outputs=10)
         tf.summary.image("eval_target", features['target'], max_outputs=10)
-        tf.summary.image("eval_transfer_sigmoid", generated, max_outputs=10)
+        tf.summary.image("eval_transfered", generated, max_outputs=10)
 
     if mode == tf.estimator.ModeKeys.TRAIN:
         loss = g_loss + d_loss
@@ -112,19 +151,17 @@ def model_fn(features, labels, mode, params):
         # EVAL
         loss = pixel_loss
 
-    theta_d = []
-    theta_g = []
+    lr = params['learning_rate']
+    g_vars, d_vars = [], []
     for v in tf.trainable_variables():
-        if v.name.startswith(D_PREFIX):
-            theta_d.append(v)
-        elif v.name.startswith(G_PREFIX):
-            theta_g.append(v)
-
-    learning_rate = params['learning_rate']
-    d_train = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(d_loss, var_list=theta_d,
-                                                                           global_step=tf.train.get_global_step())
-    g_train = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(g_loss, var_list=theta_g,
-                                                                           global_step=tf.train.get_global_step())
+        if v.name.startswith(G_PREFIX):
+            g_vars.append(v)
+        elif v.name.startswith(D_PREFIX):
+            d_vars.append(v)
+    g_train_op = tf.train.AdamOptimizer(learning_rate=lr).minimize(g_loss, var_list=g_vars,
+                                                                   global_step=tf.train.get_global_step())
+    d_train_op = tf.train.AdamOptimizer(learning_rate=lr).minimize(d_loss, var_list=d_vars,
+                                                                   global_step=tf.train.get_global_step())
 
     train_logging_hook = tf.train.LoggingTensorHook(
         {"g_loss": g_loss, "d_loss": d_loss, "cross_entropy_loss": pixel_loss}, every_n_iter=100)
@@ -143,7 +180,7 @@ def model_fn(features, labels, mode, params):
     return tf.estimator.EstimatorSpec(
         mode=mode,
         loss=loss,
-        train_op=tf.group(d_train, g_train),
+        train_op=tf.group(g_train_op, d_train_op),
         eval_metric_ops=None,
         training_hooks=[train_logging_hook, train_summary_hook],
         evaluation_hooks=[eval_logging_hook, eval_summary_hook]
--- train.py
+++ train.py
@@ -1,25 +1,24 @@
+import argparse
+import tensorflow as tf
 from model.HAN import model_fn
 from model.dataset import input_fn
 from model.io import dump_image
-import tensorflow as tf
-import argparse
+from subprocess import call
 
-# Parse command line arguments
 parser = argparse.ArgumentParser(description='Train HAN using pickled objects')
-parser.add_argument('--lr', dest='lr', type=float, default=0.0002, help='learning rate')
-parser.add_argument('--steps', dest='steps', type=int, default=None, help='max training steps')
+parser.add_argument('--lr', dest='lr', type=float, default=0.0002,
+                    help='learning rate')
+parser.add_argument('--steps', dest='steps', type=int, default=2000,
+                    help='max training steps')
 args = parser.parse_args()
 
-tf.logging.set_verbosity(tf.logging.INFO)
-
 model_params = {"learning_rate": args.lr}
-
 nn = tf.estimator.Estimator(model_fn=model_fn, params=model_params, model_dir="./model_dir/HAN")
 
-nn.train(input_fn=input_fn("./model_dir/train.obj"), steps=2)
-
+tf.logging.set_verbosity(tf.logging.INFO)
+nn.train(input_fn=input_fn("./model_dir/train.obj"), max_steps=args.steps)
 
 transfers = nn.predict(input_fn=input_fn("./model_dir/val.obj", shuffle=False, num_epochs=1))
-
 for i, t in enumerate(transfers):
     dump_image('./model_dir/%s.png' % i, t["g"])
+
